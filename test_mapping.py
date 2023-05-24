828da9ff6a443d0a62c6aa2fc03c0e6f37068b95
Augustin Desombre <augustin.desombre@polytechnique.edu>
2023-05-24 10:11:20 +0200

    Latest news

--- test_mapping.py
+++ test_mapping.py
@@ -11,4 +11,4 @@ print(map.cell_from_pos(
                 [states["stateEstimate.x"], states["stateEstimate.y"]]))
 
 map.display_map_using_cv(states)
-cv2.waitKey(2000)
+cv2.waitKey(0) #press some key to close the window
